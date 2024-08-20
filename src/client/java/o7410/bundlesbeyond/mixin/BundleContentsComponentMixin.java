2e423f369cf8653209225803fddff6493f9ef9e6
7410 <85879080+O7410@users.noreply.github.com>
2024-08-20 15:40:23 +0300

    v1.1 - Made size auto adjust to the amount of unique items

--- src/client/java/o7410/bundlesbeyond/mixin/BundleContentsComponentMixin.java
+++ src/client/java/o7410/bundlesbeyond/mixin/BundleContentsComponentMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BundleContentsComponent.class)
-public class BundleContentsComponentMixin {
+public abstract class BundleContentsComponentMixin {
     @Inject(method = "getNumberOfStacksShown", at = @At("HEAD"), cancellable = true)
     public void changeNumberOfStacksShown(CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(((BundleContentsComponent) (Object) this).size());
--- src/client/java/o7410/bundlesbeyond/mixin/BundleTooltipComponentMixin.java
+++ src/client/java/o7410/bundlesbeyond/mixin/BundleTooltipComponentMixin.java
@@ -1,18 +1,24 @@
 package o7410.bundlesbeyond.mixin;
 
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.tooltip.BundleTooltipComponent;
 import net.minecraft.component.type.BundleContentsComponent;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BundleTooltipComponent.class)
-public class BundleTooltipComponentMixin {
+public abstract class BundleTooltipComponentMixin {
     @Shadow @Final private BundleContentsComponent bundleContents;
+
+    @Unique
+    private int getModifiedColumnCount() {
+        return Math.max(4, MathHelper.ceil(Math.sqrt(this.bundleContents.size())));
+    }
 
     @Inject(method = "getNumVisibleSlots", at = @At("HEAD"), cancellable = true)
     private void changeNumberOfVisibleSlots(CallbackInfoReturnable<Integer> cir) {
@@ -22,5 +28,49 @@ public class BundleTooltipComponentMixin {
     @ModifyVariable(method = "drawNonEmptyTooltip", at = @At("STORE"))
     private boolean changeIfToDrawExtraItemsCount(boolean original) {
         return false;
+    }
+
+    @ModifyConstant(
+            method = "drawNonEmptyTooltip",
+            constant = @Constant(intValue = 4),
+            slice = @Slice(
+                    to = @At(
+                            value = "INVOKE",
+                            target = "Lnet/minecraft/client/gui/tooltip/BundleTooltipComponent;drawSelectedItemTooltip(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/client/gui/DrawContext;II)V"
+                    )
+            )
+    )
+    private int modifyDrawnColumnsCount(int constant) {
+        return this.getModifiedColumnCount();
+    }
+
+    @ModifyConstant(method = "drawNonEmptyTooltip", constant = @Constant(intValue = 96))
+    private int modifyRightAlignmentForItems(int constant) {
+        return 24 * this.getModifiedColumnCount();
+    }
+
+    @Inject(method = "getWidth", at = @At("HEAD"), cancellable = true)
+    private void changeTooltipWidth(TextRenderer textRenderer, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(24 * this.getModifiedColumnCount());
+    }
+
+    @ModifyConstant(method = "getProgressBarFill", constant = @Constant(intValue = 94))
+    private int changeProgressBarFill(int constant) {
+        return 24 * this.getModifiedColumnCount() - 2;
+    }
+
+    @ModifyConstant(method = "getRows", constant = @Constant(intValue = 4))
+    private int modifyItemsPerRow(int constant) {
+        return this.getModifiedColumnCount();
+    }
+
+    @ModifyConstant(method = "drawProgressBar", constant = @Constant(intValue = 96))
+    private int changeProgressBorderWidth(int constant) {
+        return 24 * this.getModifiedColumnCount();
+    }
+
+    @ModifyConstant(method = "drawProgressBar", constant = @Constant(intValue = 48))
+    private int changeTextCenterHorizontalOffset(int constant) {
+        return 12 * this.getModifiedColumnCount();
     }
 }
